914077d738ceaf081877dd9dfe8f3c57216cd635
Samuel Grigolato <samuel.grigolato@gmail.com>
2014-03-06 22:42:52 -0300

    Move phantomjs-qunit integration to another project #3

--- runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# verify if phantomjs is present in the system
-# http://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
-command -v phantomjs >/dev/null 2>&1 || { echo >&2 " =[ please install PhantomJS before proceeding."; exit 1; }
-
-exit 0;
-
