e04c3a09b11bd0562c0705f5a252ad7833e5c327
Samuel Grigolato <samuel.grigolato@gmail.com>
2014-03-27 21:26:53 -0300

    Implement server redirect when root url #11

--- web.js
+++ web.js
@@ -4,7 +4,19 @@ var static = require('node-static');
 var folder = new(static.Server)();
 
 require('http').createServer(function (request, response) {
+  
+  if (request.url == "/") {
+    
+    response.statusCode = 301; // redirect
+    response.setHeader('Location', 'pagy.html');
+    response.end();
+    
+  } else {
+
     request.addListener('end', function () {
-        folder.serve(request, response);
+	folder.serve(request, response);
     }).resume();
+  
+  }
+    
 }).listen(process.env.PORT || 5000);
